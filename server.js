0fe853cd4619f5287fb1bc6e840985a7ef50d793
Max <myugai@optimal-dynamics.com>
2018-11-07 10:09:38 +0600

    added extra header content-disposition

--- server.js
+++ server.js
@@ -13,17 +13,20 @@ app.get("/kurjun/rest/raw/id", function(req, res) {
     var name = req.query["name"];
     res.type("text/plain");
     res.sendFile("id/" + name, {root: "./"});
+    res.setHeader('Content-disposition', 'attachment; filename' + name);
 });
 
 app.get("/kurjun/rest/raw/info", function(req, res) {
     var name = req.query["name"];
     res.type("text/plain");
     res.sendFile("info/" + name, {root: "./"});
+    res.setHeader('Content-disposition', 'attachment; filename' + name);
 });
 
 app.get("/kurjun/rest/raw/download", function(req, res) {
     var name = req.query["name"];
     var id = req.query["id"];
+    res.setHeader('Content-disposition', 'attachment; filename' + name);
     
     if (name) {
         res.sendFile("download/" + name, {root: "./"});
